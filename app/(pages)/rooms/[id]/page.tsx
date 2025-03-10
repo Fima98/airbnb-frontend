8a931df3db46d831673df79e9e29f4be013cbc50
Fima <loboda.fima@icloud.com>
2025-03-10 12:20:32 +0200

    Detail page for property

--- app/(pages)/rooms/[id]/page.tsx
+++ app/(pages)/rooms/[id]/page.tsx
@@ -1,26 +1,29 @@
 import ReservationSidevar from "@/components/properties/ReservationSidevar";
 import Image from "next/image";
 
-const RoomDetailPage = () => {
+import apiService from "@/services/apiService";
+
+const RoomDetailPage = async ({ params }: { params: { id: string } }) => {
+    const property = await apiService.get(`/api/properties/${params.id}/`);
+    console.log(property);
     return (
         <main className="max-w-[1440px] px-10 mb-12">
-            <h1 className="text-3xl pb-4">Room detail, idk</h1>
+            {/* <h1 className="text-3xl pb-4">Room detail, idk</h1> */}
             <div className="w-full h-[56vh] overflow-hidden relative rounded-xl mb-4">
                 <Image
                     fill
                     className="object-cover w-full h-full"
-                    src={"/property_image.jpg"}
-                    alt="property"
+                    src={property.image_url}
+                    alt={property.title}
                 />
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-5 gap-4 mt-6">
                 <div className="col-span-3 pr-12">
-                    <h1 className="text-2xl font-semibold">
-                        Room name jgdhjgdjkhgdk
-                    </h1>
+                    <h1 className="text-2xl font-semibold">{property.title}</h1>
                     <span className="text-gray-500 block text-sm mb-6">
-                        6 guests · 3 bedrooms · 2 beds · 1 baths
+                        {property.guests} guests · {property.bedrooms} bedrooms
+                        · {property.bathrooms} bathrooms
                     </span>
                     <hr />
                     <div className="py-6 flex items-center space-x-4">
@@ -32,17 +35,15 @@ const RoomDetailPage = () => {
                             className="rounded-full"
                         />
                         <p className="text-sm font-semibold">
-                            Hosted by John Doe
+                            Hosted by {property.host.name}
                         </p>
                     </div>
                     <hr />
                     <div className="py-6 text-sm font-medium">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quisquam aspernatur, voluptatibus, dolores, quae, quia,
-                        voluptate quos
+                        {property.description}
                     </div>
                 </div>
-                <ReservationSidevar />
+                <ReservationSidevar property={property} />
             </div>
         </main>
     );

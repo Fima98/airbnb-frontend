7ef85abb1d359f9e1486f9128340fbb1b212ebcb
Fima <loboda.fima@icloud.com>
2025-03-23 11:40:29 +0200

    Set a property as favorite

--- components/properties/PropertyList.tsx
+++ components/properties/PropertyList.tsx
@@ -3,12 +3,14 @@
 import { useEffect, useState } from "react";
 import PropertyListItem from "./PropertyListItem";
 import apiService from "@/services/apiService";
+import { is } from "date-fns/locale";
 
 export type PropertyType = {
     id: string;
     title: string;
     price_per_night: number;
     image_url: string;
+    is_favorite: boolean;
 };
 
 interface PropertyListProps {
@@ -18,6 +20,23 @@ interface PropertyListProps {
 const PropertyList: React.FC<PropertyListProps> = ({ hostId }) => {
     const [properties, setProperties] = useState<PropertyType[]>([]);
 
+    const markFavorite = (id: string, isFavorite: boolean) => {
+        const Properties = properties.map((property: PropertyType) => {
+            if (property.id === id) {
+                property.is_favorite = isFavorite;
+
+                if (isFavorite) {
+                    console.log("Favorite");
+                } else {
+                    console.log("Unfavorite");
+                }
+            }
+            return property;
+        });
+
+        setProperties(Properties);
+    };
+
     const getProperties = async () => {
         let url = "/api/properties/";
         if (hostId) {
@@ -25,17 +44,31 @@ const PropertyList: React.FC<PropertyListProps> = ({ hostId }) => {
         }
         const response = await apiService.get(url);
         console.log(response);
-        setProperties(response);
+        setProperties(
+            response.data.map((property: PropertyType) => {
+                if (response.favorites.includes(property.id)) {
+                    property.is_favorite = true;
+                } else {
+                    property.is_favorite = false;
+                }
+                return property;
+            })
+        );
     };
 
     useEffect(() => {
         getProperties();
     }, []);
-
     return (
         <>
             {properties.map((property) => (
-                <PropertyListItem key={property.id} property={property} />
+                <PropertyListItem
+                    key={property.id}
+                    property={property}
+                    markFavorite={(isFavorite: boolean) =>
+                        markFavorite(property.id, isFavorite)
+                    }
+                />
             ))}
         </>
     );
--- components/properties/PropertyListItem.tsx
+++ components/properties/PropertyListItem.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
 import { useRouter } from "next/navigation";
+import FavoriteButton from "../forms/FavoriteButton";
 
 interface PropertyListItemProps {
     property: PropertyType;
+    markFavorite?: (isFavorite: boolean) => void;
 }
 
-const PropertyListItem: React.FC<PropertyListItemProps> = ({ property }) => {
+const PropertyListItem: React.FC<PropertyListItemProps> = ({
+    property,
+    markFavorite,
+}) => {
     const router = useRouter();
     return (
         <div
@@ -23,6 +28,16 @@ const PropertyListItem: React.FC<PropertyListItemProps> = ({ property }) => {
                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     className="hover:scale-110 object-cover transition h-full w-full"
                 />
+
+                {markFavorite && (
+                    <FavoriteButton
+                        id={property.id}
+                        isFavorite={property.is_favorite}
+                        markFavorite={(isFavorite: boolean) =>
+                            markFavorite(isFavorite)
+                        }
+                    />
+                )}
             </div>
             <div className="mt-2">
                 <p className="text-sm font-semibold">{property.title}</p>

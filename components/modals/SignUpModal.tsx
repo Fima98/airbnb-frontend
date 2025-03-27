423f58b03624c6574981a20765d1cbdb4d2f347b
Fima <loboda.fima@icloud.com>
2025-03-27 08:15:27 -0700

    Add first/last name fields and fix reservation date handling

--- components/modals/SignUpModal.tsx
+++ components/modals/SignUpModal.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Modal from "./Modal";
-
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import useSignUpModal from "@/hooks/useSignUpModal";
@@ -10,87 +9,97 @@ import apiService from "@/services/apiService";
 import { handleLogin } from "@/lib/actions";
 
 const SignUpModal = () => {
-    const router = useRouter();
-    const signUpModal = useSignUpModal();
-    const [email, setEmail] = useState("");
-    const [password1, setPassword1] = useState("");
-    const [password2, setPassword2] = useState("");
-    const [errors, setError] = useState<string[]>([]);
+  const router = useRouter();
+  const signUpModal = useSignUpModal();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [errors, setError] = useState<string[]>([]);
 
-    const submitSignUp = async () => {
-        const formData = {
-            email: email,
-            password1: password1,
-            password2: password2,
-        };
+  const submitSignUp = async () => {
+    const fullName = `${firstName} ${lastName}`.trim();
 
-        const response = await apiService.post("/api/auth/register/", formData);
-        console.log(response);
-
-        if (response.access) {
-            await handleLogin(
-                response.user.pk,
-                response.access,
-                response.refresh
-            );
-            signUpModal.close();
-            router.push("/");
-        } else {
-            const errors: string[] = Object.values(response).map(
-                (error: any) => {
-                    return error;
-                }
-            );
-            setError(errors);
-        }
+    const formData = {
+      name: fullName,
+      email: email,
+      password1: password1,
+      password2: password2,
     };
 
-    const content = (
-        <div className="">
-            <h2 className="mb-6 text-2xl font-semibold">Welcome to Airdnd</h2>
-            <form action={submitSignUp} className="space-y-4">
-                <input
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="email"
-                    className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
-                    placeholder="Email"
-                />
-                <input
-                    onChange={(e) => setPassword1(e.target.value)}
-                    type="password"
-                    className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
-                    placeholder="Password"
-                />
-                <input
-                    onChange={(e) => setPassword2(e.target.value)}
-                    type="password"
-                    className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
-                    placeholder="Repeat password"
-                />
+    const response = await apiService.post("/api/auth/register/", formData);
+    console.log(response);
 
-                {errors.map((error, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className="p-5 bg-airbnb text-white rounded-xl opacity-60"
-                        >
-                            {error}
-                        </div>
-                    );
-                })}
+    if (response.access) {
+      await handleLogin(response.user.id, response.access, response.refresh);
+      signUpModal.close();
+      router.push("/");
+    } else {
+      const errors: string[] = Object.values(response).map(
+        (error: any) => error
+      );
+      setError(errors);
+    }
+  };
 
-                <SubmitButton label="Continue" className="w-full" />
-            </form>
-        </div>
-    );
-    return (
-        <Modal
-            isOpen={signUpModal.isOpen}
-            label="Sign up"
-            content={content}
-            close={signUpModal.close}
+  const content = (
+    <div className="">
+      <h2 className="mb-6 text-2xl font-semibold">Welcome to Airdnd</h2>
+      <form action={submitSignUp} className="space-y-4">
+        <input
+          onChange={(e) => setFirstName(e.target.value)}
+          type="text"
+          className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
+          placeholder="First Name"
         />
-    );
+        <input
+          onChange={(e) => setLastName(e.target.value)}
+          type="text"
+          className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
+          placeholder="Last Name"
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
+          placeholder="Email"
+        />
+        <input
+          onChange={(e) => setPassword1(e.target.value)}
+          type="password"
+          className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
+          placeholder="Password"
+        />
+        <input
+          onChange={(e) => setPassword2(e.target.value)}
+          type="password"
+          className="w-full h-[54px] px-4 border border-gray-100 rounded-xl"
+          placeholder="Repeat password"
+        />
+
+        {errors.map((error, index) => (
+          <div
+            key={index}
+            className="p-5 bg-airbnb text-white rounded-xl opacity-60"
+          >
+            {error}
+          </div>
+        ))}
+
+        <SubmitButton label="Continue" className="w-full" />
+      </form>
+    </div>
+  );
+
+  return (
+    <Modal
+      isOpen={signUpModal.isOpen}
+      label="Sign up"
+      content={content}
+      close={signUpModal.close}
+    />
+  );
 };
 
 export default SignUpModal;
--- components/properties/ReservationSidevar.tsx
+++ components/properties/ReservationSidevar.tsx
@@ -9,225 +9,233 @@ import apiService from "@/services/apiService";
 import useLoginModal from "@/hooks/useLoginModal";
 
 const initialDateRange = {
-    startDate: new Date(),
-    endDate: new Date(),
-    key: "selection",
+  startDate: new Date(),
+  endDate: new Date(),
+  key: "selection",
 };
 
 export type Property = {
-    id: string;
-    price_per_night: number;
-    guests: number;
+  id: string;
+  price_per_night: number;
+  guests: number;
 };
 
 type ReservationSidevarProps = {
-    userId: string | null;
-    property: Property;
+  userId: string | null;
+  property: Property;
 };
 
 const ReservationSidevar: React.FC<ReservationSidevarProps> = ({
-    property,
-    userId,
+  property,
+  userId,
 }) => {
-    const loginModal = useLoginModal();
+  const loginModal = useLoginModal();
 
-    const [fee, setFee] = useState<number>(0);
-    const [nights, setNights] = useState<number>(1);
-    const [total, setTotal] = useState<number>(0);
-    const [dateRange, setDateRange] = useState<Range>(initialDateRange);
-    const [guests, setGuests] = useState<number>(1);
-    const [showCalendar, setShowCalendar] = useState<boolean>(false);
-    const [reservedDates, setReservedDates] = useState<Date[]>([]);
+  const [fee, setFee] = useState<number>(0);
+  const [nights, setNights] = useState<number>(1);
+  const [total, setTotal] = useState<number>(0);
+  const [dateRange, setDateRange] = useState<Range>(initialDateRange);
+  const [guests, setGuests] = useState<number>(1);
+  const [showCalendar, setShowCalendar] = useState<boolean>(false);
+  const [reservedDates, setReservedDates] = useState<Date[]>([]);
 
-    const guestsRange = Array.from(
-        { length: property.guests },
-        (_, i) => i + 1
-    );
+  const guestsRange = Array.from({ length: property.guests }, (_, i) => i + 1);
 
-    const handleReservation = async () => {
-        if (userId) {
-            if (dateRange.startDate && dateRange.endDate) {
-                const formData = new FormData();
-                formData.append("guests", guests.toString());
-                formData.append(
-                    "start_date",
-                    format(dateRange.startDate, "yyyy-MM-dd")
-                );
-                formData.append(
-                    "end_date",
-                    format(dateRange.endDate, "yyyy-MM-dd")
-                );
-
-                const response = await apiService.post(
-                    `/api/properties/${property.id}/reserve/`,
-                    formData
-                );
-
-                if (response.success) {
-                    console.log(response);
-                } else {
-                    console.log("Error:", response);
-                }
-            }
-        } else {
-            loginModal.open();
-        }
-    };
-
-    const getReservations = async () => {
-        const reservations = await apiService.get(
-            `/api/properties/${property.id}/reservations/`
+  const handleReservation = async () => {
+    if (userId) {
+      if (dateRange.startDate && dateRange.endDate) {
+        const formData = new FormData();
+        formData.append("guests", guests.toString());
+        formData.append(
+          "start_date",
+          format(dateRange.startDate, "yyyy-MM-dd")
         );
-        console.log(`Reservations:`, reservations);
+        formData.append("end_date", format(dateRange.endDate, "yyyy-MM-dd"));
 
-        let dates: Date[] = [];
-        reservations.forEach((reservation: any) => {
-            const start = new Date(reservation.start_date);
-            const end = new Date(reservation.end_date);
+        console.log(dateRange.startDate, dateRange.endDate);
 
-            for (
-                let d = new Date(start);
-                d <= end;
-                d.setDate(d.getDate() + 1)
-            ) {
-                dates.push(new Date(d));
-            }
-        });
+        const response = await apiService.post(
+          `/api/properties/${property.id}/reserve/`,
+          formData
+        );
 
-        setReservedDates(dates);
-    };
-
-    useEffect(() => {
-        getReservations();
-    }, []);
-
-    useEffect(() => {
-        if (dateRange.startDate && dateRange.endDate) {
-            const days = differenceInDays(
-                dateRange.endDate,
-                dateRange.startDate
-            );
-
-            if (days && property.price_per_night) {
-                const _fee = ((days * property.price_per_night) / 100) * 5;
-                setFee(_fee);
-                setTotal(days * property.price_per_night + _fee);
-                setNights(days);
-            } else {
-                const _fee = (property.price_per_night / 100) * 5;
-                setFee(_fee);
-                setTotal(property.price_per_night + _fee);
-                setNights(1);
-            }
+        if (response.success) {
+          console.log(response);
+          getReservations();
+        } else {
+          console.log("Error:", response);
         }
-    }, [dateRange, property.price_per_night]);
+      }
+    } else {
+      loginModal.open();
+    }
+  };
 
-    const _setDateRange = (ranges: any) => {
-        const { selection } = ranges || {};
-        if (!selection?.startDate || !selection?.endDate) {
-            return;
-        }
-
-        const newStartDate = new Date(selection.startDate);
-        const newEndDate = new Date(selection.endDate);
-
-        if (newEndDate <= newStartDate) {
-            newEndDate.setDate(newStartDate.getDate() + 1);
-        }
-
-        setDateRange({
-            startDate: newStartDate,
-            endDate: newEndDate,
-            key: "selection",
-        });
-    };
-
-    return (
-        <aside className="p-6 mt-2 col-span-2 rounded-xl border border-gray-200 shadow-xl">
-            <div className="border relative border-gray-400 rounded-xl cursor-pointer">
-                <div
-                    className="flex items-center border-b border-gray-400 px-3 cursor-pointer"
-                    onClick={() => setShowCalendar(!showCalendar)}
-                >
-                    <div className="flex-1 w-1/2 border-r py-3 border-gray-400">
-                        <h3 className="text-xs font-bold">CHECK-IN</h3>
-                        <span className="text-sm">
-                            {format(
-                                dateRange.startDate || new Date(),
-                                "dd/MM/yyyy"
-                            )}
-                        </span>
-                    </div>
-
-                    <div className="flex-1 text-right py-3">
-                        <h3 className="text-xs font-bold">CHECK-OUT</h3>
-                        <span className="text-sm">
-                            {format(
-                                dateRange.endDate || new Date(),
-                                "dd/MM/yyyy"
-                            )}
-                        </span>
-                    </div>
-                </div>
-
-                <div className="p-3">
-                    <label className="block font-bold text-xs" htmlFor="guests">
-                        GUESTS
-                    </label>
-                    <select
-                        id="guests"
-                        value={guests}
-                        onChange={(e) => setGuests(parseInt(e.target.value))}
-                        className="w-full text-sm mt-1 rounded-md"
-                    >
-                        {guestsRange.map((guest) => (
-                            <option key={guest} value={guest}>
-                                {guest} {guest === 1 ? "guest" : "guests"}
-                            </option>
-                        ))}
-                    </select>
-                </div>
-            </div>
-            {showCalendar && (
-                <div className="mt-4 bg-white rounded-xl border border-gray-400 top-0 w-full">
-                    <Calendar
-                        value={dateRange}
-                        onChange={(ranges) => {
-                            _setDateRange(ranges);
-                        }}
-                        reservedDates={reservedDates}
-                    />
-                </div>
-            )}
-
-            <button
-                onClick={handleReservation}
-                className="w-full mb-6 py-3 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl mt-4"
-            >
-                Reserve
-            </button>
-
-            <div className="mb-4 flex justify-center">
-                <p>You won’t be charged yet</p>
-            </div>
-            <div className="mb-4 flex justify-between align-center items-center">
-                <p>
-                    ${property.price_per_night} x {nights}{" "}
-                    {nights === 1 ? "night" : "nights"}
-                </p>
-                <p>${property.price_per_night * nights}</p>
-            </div>
-            <div className="mb-4 flex justify-between align-center items-center">
-                <p>Cleaning fee</p>
-                <p>${fee.toFixed(2)}</p>
-            </div>
-            <hr />
-            <div className="mt-4 flex justify-between align-center items-center">
-                <p className="font-semibold">Total before taxes</p>
-                <p className="font-semibold">${total.toFixed(2)}</p>
-            </div>
-        </aside>
+  const getReservations = async () => {
+    const reservations = await apiService.get(
+      `/api/properties/${property.id}/reservations/`
     );
+    console.log(`Reservations:`, reservations);
+
+    let dates: Date[] = [];
+    reservations.forEach((reservation: any) => {
+      // Parse the start and end dates as UTC dates
+      const start = new Date(
+        Date.UTC(
+          parseInt(reservation.start_date.substring(0, 4)), // Year
+          parseInt(reservation.start_date.substring(5, 7)) - 1, // Month (zero-based)
+          parseInt(reservation.start_date.substring(8, 10)) // Day
+        )
+      );
+
+      const end = new Date(
+        Date.UTC(
+          parseInt(reservation.end_date.substring(0, 4)), // Year
+          parseInt(reservation.end_date.substring(5, 7)) - 1, // Month (zero-based)
+          parseInt(reservation.end_date.substring(8, 10)) // Day
+        )
+      );
+
+      // Fix for the local timezone causing the shift by a day
+      // Set the time to noon UTC to avoid it rolling over to the previous day
+      start.setUTCHours(12, 0, 0, 0);
+      end.setUTCHours(12, 0, 0, 0);
+
+      // Iterate through the range of dates
+      for (
+        let d = new Date(start);
+        d <= end;
+        d.setUTCDate(d.getUTCDate() + 1)
+      ) {
+        dates.push(new Date(d));
+      }
+    });
+
+    setReservedDates(dates);
+  };
+
+  useEffect(() => {
+    getReservations();
+  }, []);
+
+  useEffect(() => {
+    if (dateRange.startDate && dateRange.endDate) {
+      const days = differenceInDays(dateRange.endDate, dateRange.startDate);
+
+      if (days && property.price_per_night) {
+        const _fee = ((days * property.price_per_night) / 100) * 5;
+        setFee(_fee);
+        setTotal(days * property.price_per_night + _fee);
+        setNights(days);
+      } else {
+        const _fee = (property.price_per_night / 100) * 5;
+        setFee(_fee);
+        setTotal(property.price_per_night + _fee);
+        setNights(1);
+      }
+    }
+  }, [dateRange, property.price_per_night]);
+
+  const _setDateRange = (ranges: any) => {
+    const { selection } = ranges || {};
+    if (!selection?.startDate || !selection?.endDate) {
+      return;
+    }
+
+    const newStartDate = new Date(selection.startDate);
+    const newEndDate = new Date(selection.endDate);
+
+    if (newEndDate <= newStartDate) {
+      newEndDate.setDate(newStartDate.getDate() + 1);
+    }
+
+    setDateRange({
+      startDate: newStartDate,
+      endDate: newEndDate,
+      key: "selection",
+    });
+  };
+
+  return (
+    <aside className="p-6 mt-2 col-span-2 rounded-xl border border-gray-200 shadow-xl">
+      <div className="border relative border-gray-400 rounded-xl cursor-pointer">
+        <div
+          className="flex items-center border-b border-gray-400 px-3 cursor-pointer"
+          onClick={() => setShowCalendar(!showCalendar)}
+        >
+          <div className="flex-1 w-1/2 border-r py-3 border-gray-400">
+            <h3 className="text-xs font-bold">CHECK-IN</h3>
+            <span className="text-sm">
+              {format(dateRange.startDate || new Date(), "dd/MM/yyyy")}
+            </span>
+          </div>
+
+          <div className="flex-1 text-right py-3">
+            <h3 className="text-xs font-bold">CHECK-OUT</h3>
+            <span className="text-sm">
+              {format(dateRange.endDate || new Date(), "dd/MM/yyyy")}
+            </span>
+          </div>
+        </div>
+
+        <div className="p-3">
+          <label className="block font-bold text-xs" htmlFor="guests">
+            GUESTS
+          </label>
+          <select
+            id="guests"
+            value={guests}
+            onChange={(e) => setGuests(parseInt(e.target.value))}
+            className="w-full text-sm mt-1 rounded-md"
+          >
+            {guestsRange.map((guest) => (
+              <option key={guest} value={guest}>
+                {guest} {guest === 1 ? "guest" : "guests"}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      {showCalendar && (
+        <div className="mt-4 bg-white rounded-xl border border-gray-400 top-0 w-full">
+          <Calendar
+            value={dateRange}
+            onChange={(ranges) => {
+              _setDateRange(ranges);
+            }}
+            reservedDates={reservedDates}
+          />
+        </div>
+      )}
+
+      <button
+        onClick={handleReservation}
+        className="w-full mb-6 py-3 text-center text-white bg-airbnb hover:bg-airbnb-dark rounded-xl mt-4"
+      >
+        Reserve
+      </button>
+
+      <div className="mb-4 flex justify-center">
+        <p>You won’t be charged yet</p>
+      </div>
+      <div className="mb-4 flex justify-between align-center items-center">
+        <p>
+          ${property.price_per_night} x {nights}{" "}
+          {nights === 1 ? "night" : "nights"}
+        </p>
+        <p>${property.price_per_night * nights}</p>
+      </div>
+      <div className="mb-4 flex justify-between align-center items-center">
+        <p>Cleaning fee</p>
+        <p>${fee.toFixed(2)}</p>
+      </div>
+      <hr />
+      <div className="mt-4 flex justify-between align-center items-center">
+        <p className="font-semibold">Total before taxes</p>
+        <p className="font-semibold">${total.toFixed(2)}</p>
+      </div>
+    </aside>
+  );
 };
 
 export default ReservationSidevar;
